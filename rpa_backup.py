05bb0dbb9e87df1d934b47b1cda37f74c20d609b
Zain Babur <60311791+zainbabur@users.noreply.github.com>
2020-02-02 22:54:52 +0500

    Update rpa_backup.py

--- rpa_backup.py
+++ rpa_backup.py
@@ -2,20 +2,22 @@ import os
 from datetime import datetime, timedelta
 import time
 
+
 files_folder = "C:\\Users\\Zain Babur\\Documents\\"
 bat_folder = "D:\\"
 
 def check_files(filepath, batpath, delta = 2, waittime = 180):
-    mtime = os.path.getmtime(filepath)
-    dif = datetime.now().replace(microsecond=0)-datetime.fromtimestamp(mtime).replace(microsecond=0)
-    dif_hour = dif.seconds/3600
-    while dif_hour > delta:
-        os.system(batpath)
-        time.sleep(waittime)
-        mtime = os.path.getmtime(filepath)
-        dif = datetime.now().replace(microsecond=0)-datetime.fromtimestamp(mtime).replace(microsecond=0)
-        dif_hour = dif.seconds/3600
+    mtime = os.path.getmtime(filepath) #get modified time of the file
+    dif = datetime.now().replace(microsecond=0)-datetime.fromtimestamp(mtime).replace(microsecond=0) #calculate difference of modified time from now
+    dif_hour = dif.seconds/3600 #get number of hours passed
+    while dif_hour > delta: #until file is updated, keep running the batch file
+        os.system(batpath) #run batch file
+        time.sleep(waittime) #python waits for the RPA to finish running but we still need to wait for the file to be updated, which is done through other python scripts
+        mtime = os.path.getmtime(filepath) #get modified time again
+        dif = datetime.now().replace(microsecond=0)-datetime.fromtimestamp(mtime).replace(microsecond=0) #get difference
+        dif_hour = dif.seconds/3600 
 
+#do this process for multiple files
 check_files(filepath = files_folder+"DailyProgress.xlsx", batpath = bat_folder+"ProgressRPA.bat")
 check_files(filepath = files_folder+"PlannedActivities.xlsx", batpath = bat_folder+"PlanRPA.bat", waittime=240)
 check_files(filepath = files_folder+"AccessIssues.xlsx", batpath = bat_folder+"IssuesRPA.bat", delta=1)
